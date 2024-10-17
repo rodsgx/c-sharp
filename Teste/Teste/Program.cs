8d94f396e77167bd602074fe4dc31e8f88fe6dba
Rodrigo Souza <rodsg.x@gmail.com>
2024-10-17 13:42:21 -0300

    Organizando projeto

--- Teste/Teste/Program.cs
+++ Teste/Teste/Program.cs
@@ -1,2 +1,12 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Teste
+{
+    class Progra
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
