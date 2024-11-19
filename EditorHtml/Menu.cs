57766f62974606bc6bab33b25cb5d9bb6ccb5a66
Rodrigo Souza <rodsg.x@gmail.com>
2024-11-18 22:53:18 -0300

    EditorHtml finalizado

--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection.Metadata;
 
 namespace EditorHtml
 {
@@ -11,6 +12,9 @@ namespace EditorHtml
             Console.ForegroundColor = ConsoleColor.Black;
 
             DrawScreen();
+            WriteOptions();
+            var option = short.Parse(Console.ReadLine());
+            HandleMenuOption(option);
         }
 
         public static void DrawScreen()
@@ -37,6 +41,40 @@ namespace EditorHtml
 
             Console.Write("+");
             Console.Write("\n");
+        }
+
+        public static void WriteOptions()
+        {
+            Console.SetCursorPosition(3, 2);
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(3, 3);
+            Console.WriteLine("=============");
+            Console.SetCursorPosition(3, 4);
+            Console.WriteLine("Selecione uma opção abaixo:");
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("1 - Novo arquivo");
+            Console.SetCursorPosition(3, 7);
+            Console.WriteLine("2 - Abrir");
+            Console.SetCursorPosition(3, 9);
+            Console.WriteLine("0 - Sair");
+            Console.SetCursorPosition(3, 10);
+            Console.Write("Opção: ");
+        }
+
+        public static void HandleMenuOption(short option)
+        {
+            switch (option)
+            {
+                case 1: Editor.Show(); break;
+                case 2: Console.WriteLine("View"); break;
+                case 0:
+                    {
+                        Console.Clear();
+                        Environment.Exit(0);
+                        break;
+                    }
+                default: Show(); break;
+            }
         }
     }
 }
